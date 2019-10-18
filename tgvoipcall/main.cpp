9e9a6295755721fc794ee019321e4427569264fa
Alexander Lunegov <alunegov@gmail.com>
2019-10-18 15:56:13 +0500

    wip

--- tgvoipcall/main.cpp
+++ tgvoipcall/main.cpp
@@ -13,41 +13,26 @@ struct Conf {
     static const bool AllowP2p{false};
     static const int32_t ConnMaxLayer{0};
     std::string AddrIp;
-    uint16_t AddrPort;
+    uint16_t AddrPort{0};
     std::string EncriptionKey;
-    bool IsOutgoing;
+    bool IsOutgoing{false};
     std::string Tag;
     std::string InputFileName;
     std::string OutputFileName;
     std::string ConfigFileName;
 };
 
-
-Conf parseConf(int argc, const char* argv[]) {
-    Conf res;
-    const auto addr = argv[1];
-    const auto tag = argv[2];
-    const auto encryption_key = argv[3];  // -k
-    const auto input_file_name = argv[4];  // -i
-    const auto output_file_name = argv[5];  // -o
-    const auto config_file_name = argv[6];  // -c
-    const auto dir = argv[7];  // -r
-
-    const std::string addr_ip{""};
-    const uint16_t addr_port{0};
-    const bool dir_is_outgoing{false};
-    return res;
-}
+Conf parseArgs(int argc, const char* argv[]);
+std::string readConfig(std::string ConfigFileName);
+//std::vector<Endpoint> createEndpoints(const Conf& conf);
 
 int main(int argc, const char* argv[]) {
     try {
-        const auto conf = parseConf(argc, argv);
+        const auto conf = parseArgs(argc, argv);
 
-        std::ifstream config_file{conf.ConfigFileName};
-        std::stringstream config;
-        config << config_file.rdbuf();
-
-        ServerConfig::GetSharedInstance()->Update(config.str());
+        const auto config = readConfig(conf.ConfigFileName);
+        cout << config << std::endl;
+        /*ServerConfig::GetSharedInstance()->Update(config);
 
         auto c = std::make_unique<VoIPController>();
 
@@ -57,12 +42,7 @@ int main(int argc, const char* argv[]) {
         
         c->SetEncryptionKey((char*)(conf.EncriptionKey.c_str()), conf.IsOutgoing);
     
-        std::vector<Endpoint> endpoints;
-
-        const Endpoint endpoint{conf.EndpointId, conf.AddrPort, IPv4Address{conf.AddrIp}, IPv6Address{},
-                Endpoint::UDP_RELAY, (unsigned char*)(conf.Tag.c_str())};
-        endpoints.emplace_back(endpoint);
-
+        const auto endpoints = createEndpoints(conf);
         c->SetRemoteEndpoints(endpoints, conf.AllowP2p, conf.ConnMaxLayer);
 
         c->Start();
@@ -74,9 +54,65 @@ int main(int argc, const char* argv[]) {
 
         cout << c->GetDebugLog();
 
-        c.release();
+        c.release();*/
     } catch (std::exception& e) {
         cerr << e.what();
         return -1;
     }
 }
+
+std::pair<std::string, uint16_t> parseAddr(std::string addr);
+
+Conf parseArgs(int argc, const char* argv[]) {
+    Conf res;
+
+    const auto p = parseAddr(argv[1]);
+    res.AddrIp = p.first;
+    res.AddrPort = p.second;
+
+    res.Tag = argv[2];
+
+    for (int i = 3; (i + 2) <= argc; i += 2) {
+        const std::string key{argv[i]};
+        if (key == "-k") {
+            res.EncriptionKey = argv[i + 1];
+        } else if (key == "-i") {
+            res.InputFileName = argv[i + 1];
+        } else if (key == "-o") {
+            res.OutputFileName = argv[i + 1];
+        } else if (key == "-c") {
+            res.ConfigFileName = argv[i + 1];
+        } else if (key == "-r") {
+            res.IsOutgoing = std::string{argv[i + 1]} == "caller";
+        }
+    }
+
+    return res;
+}
+
+std::pair<std::string, uint16_t> parseAddr(std::string addr) {
+    const auto p = addr.find(':');
+    if (p == string::npos) {
+        throw std::exception{"no port specified"};
+    }
+    const auto ip = addr.substr(0, p);
+    const auto port = std::stoi(addr.substr(p + 1));
+    return std::make_pair(ip, port);
+}
+
+std::string readConfig(std::string configFileName) {
+    const std::ifstream config_file{configFileName};
+    std::stringstream config;
+    config << config_file.rdbuf();
+    return config.str();
+}
+
+/*std::vector<Endpoint> createEndpoints(const Conf& conf) {
+    std::vector<Endpoint> res;
+
+    const Endpoint endpoint{conf.EndpointId, conf.AddrPort, IPv4Address{conf.AddrIp}, IPv6Address{},
+            Endpoint::UDP_RELAY, (unsigned char*)(conf.Tag.c_str())};
+    res.emplace_back(endpoint);
+
+    return res;
+}*/
